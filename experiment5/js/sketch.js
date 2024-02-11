2193e191aaca8866adf183e0aa602f6407fe8f76
cannedcorgies <artistafdo@gmail.com>
2024-02-11 15:58:47 -0800

    very easy boom implemented

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,137 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+var particles = [];
+var maxAge = 40;
+  var agingScale = 3;
+  var distanceCutoff = 20;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let mic;
+  var boomDetected = false;
+  var boomCLick = false;
+  var volumeCutoff = 70;
+  var volumeScaling = 10000;
+  var volumeReduction = 500;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+	angleMode(DEGREES);  // instead of radians
+  
+	createCanvas(windowWidth, windowHeight, WEBGL);
+	background(100);
+  
+	for(var i = 0; i<1000; i++){ particles.push(new particle);} // initial explosion
+	noStroke();
+
+  mic = new p5.AudioIn();
+  mic.start();
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+	
+    offset = 0;
+	background(51);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // ===== PARTICLES SYSTEM
+	for(var i = 0; i<particles.length; i++){    // update particles
+		particles[i].update();
+	}
+  
+	for(var i = 0; i<particles.length; i++){    // update particle list
+      
+		if(particles[i+offset].ded){
+          
+			particles.splice(i+offset, 1);
+			offset--;
+			i++;
+          
+		}
+	}
+
+
+  // ===== AUDIO DETECTION
+  let vol = mic.getLevel();
+  vol *= volumeScaling;
+  vol = round(vol);
+  if (vol > volumeCutoff) {
+
+    boomDetected = true;
+
+  } else {
+
+    boomDetected = false;
+    boomCLick = false;
+
+  }
+
+  if (boomDetected && !boomCLick) {
+
+    boomCLick = true;
+    Explode(vol);
+    console.log("BOOM DETECTED");
+
+  }
+
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+class particle{
+  
+	constructor(volume){
+      
+    var velConst = volume/volumeReduction;
+
+        // POSITION
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+      
+        // VELOCITY
+		this.vel = [random(-velConst, velConst), random(-velConst, velConst), random(-velConst, velConst)];
+		this.total = 0.5/(sqrt((this.vel[0]*this.vel[0])+(this.vel[1]*this.vel[1])+(this.vel[2]*this.vel[2]))+random(-0.9, -0.3));
+		this.vel = [this.vel[0]*this.total, this.vel[1]*this.total, this.vel[2]*this.total];
+      
+        // AGE AND MISC
+		this.color = random(50, 255);
+		this.age = 0;
+        this.maxAge = maxAge;
+        this.ded = false;
+      
+	}
+  
+	update(){
+      
+        // AGING
+        var distance = abs(this.x + this.y + this.z);
+      
+		this.age += random(0.2, 2);      // so that it sizzles...
+		if(this.ded === false && this.maxAge < this.age) { this.ded = true; }  // age limit
+        //if(this.ded === false && distance > distanceCutoff) { this.ded = true; }
+      
+        // VELOCITY MODIFICATION
+		this.vel = [this.vel[0]*random(0.99, 1.001), this.vel[1]*random(0.99, 1.001), this.vel[2]*random(0.99, 1.001)];  // more organic velocity
+		this.x += this.vel[0];
+		this.y += this.vel[1];
+		this.z += this.vel[2];
+      
+        // coloring?
+		if(this.color<240){
+			fill(255, this.color, 0);
+		}
+		else{
+			fill(this.color);
+		}
+      
+        // DISPLACEMENT
+		translate(this.x, this.y, this.z);
+		sphere(10, 8, 4);
+		translate(-this.x, -this.y, -this.z);
+	}
+}
+
+function Explode(volume) {
+  
+	for(var i = 0; i<500; i++){        // push 500 particles
+      
+		particles.push(new particle(volume));
+      
+	}
+  
 }
